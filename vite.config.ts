7d95838056a6267a9ebc57d94d988b228e093690
Mohamad Nachawati <75593627+MohamadNach@users.noreply.github.com>
2024-10-17 19:45:25 +0300

    chore : change the repository name in vite configeration file.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MohamadNachawati.github.io/', // Seting the base to the name of the repository for Github Deployment
+  base: '/MohamadNach.github.io/', // Seting the base to the name of the repository for Github Deployment
   plugins: [react()],
 });
