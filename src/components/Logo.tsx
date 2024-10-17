3748c024a64f27240b870e95c93fc0f1868ca151
Mohamad Nachawati <75593627+MohamadNach@users.noreply.github.com>
2024-10-18 00:34:11 +0300

    chore: change the logo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import logo from '../assets/Tech Channel Logo in Sky Blue White Purple Bold Style (2).png';
+import logo from '../assets/logo.png';
 const Logo = () => {
   return (
     <>
