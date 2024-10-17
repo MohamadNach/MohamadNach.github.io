3c96f235095b270b218038b20fb6ba8667e09235
Mohamad Nachawati <75593627+MohamadNach@users.noreply.github.com>
2024-10-17 19:56:27 +0300

    chore: correcte vite configeration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MohamadNach.github.io/', // Seting the base to the name of the repository for Github Deployment
+  base: '/', // Seting the base to the name of the repository for Github Deployment
   plugins: [react()],
 });
