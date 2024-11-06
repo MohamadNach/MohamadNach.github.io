d2d095cbc5c1fd6845951ac65cff04374719de91
Mohamad Nachawati <75593627+MohamadNach@users.noreply.github.com>
2024-11-06 10:17:35 +0200

    chore: adding projects images and modifying navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ import Language from './Language';
 
 const Navbar = () => {
   return (
-    <nav className=' mb-10 flex items-center justify-between py-6'>
+    <nav className=' mb-10 flex items-center justify-between'>
       <div className='flex flex-shrink-0 items-center w-52 h-28'>
         <Logo />
       </div>
-      <div className='m-8 flex items-center justify-center gap-4 text-2xl cursor-pointer'>
+      <div className='m-1 flex items-center justify-center gap-4 text-2xl cursor-pointer'>
         <a href='https://www.linkedin.com/in/mohamad-nachawati-a801891b7/'>
           <FaLinkedin />
         </a>
